75c7651bd1e2cf6a3a7519ea7e656412380af95f
Matheus Alves Arthus <matheus.arthus@gmail.com>
2020-03-27 15:45:17 -0300

    feature: Configurando StatusBar

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,7 @@ const Stack = createStackNavigator();
 
 export default function createRouter() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator headerMode="none">
       <Stack.Screen name="SignIn" component={SignIn} />
       <Stack.Screen name="SignUp" component={SignUp} />
     </Stack.Navigator>
