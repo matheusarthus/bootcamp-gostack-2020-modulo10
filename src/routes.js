991f783e28e9dfb93d8b1d94e14f41d076da845f
Matheus Alves Arthus <matheus.arthus@gmail.com>
2020-03-27 11:02:27 -0300

    feature: Configurando background

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { createStackNavigator } from '@react-navigation/stack';
+
+import SignIn from '~/pages/SignIn';
+import SignUp from '~/pages/SignUp';
+
+const Stack = createStackNavigator();
+
+export default function createRouter() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="SignIn" component={SignIn} />
+      <Stack.Screen name="SignUp" component={SignUp} />
+    </Stack.Navigator>
+  );
+}
