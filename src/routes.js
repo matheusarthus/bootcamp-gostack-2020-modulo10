bb5046e85d8acf05924f4466164c7aa59dced384
Matheus Alves Arthus <matheus.arthus@gmail.com>
2020-03-28 12:42:09 -0300

    feature: Estilização das Rotas

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,13 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import SignIn from '~/pages/SignIn';
 import SignUp from '~/pages/SignUp';
 
 import Dashboard from '~/pages/Dashboard';
+import Profile from '~/pages/Profile';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -18,8 +21,36 @@ export default function createRouter(isSigned = false) {
       <Stack.Screen name="SignUp" component={SignUp} />
     </Stack.Navigator>
   ) : (
-    <Tab.Navigator>
-      <Tab.Screen name="Dashboard" component={Dashboard} />
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: '#fff',
+        inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
+        style: {
+          backgroundColor: '#8d41a8',
+        },
+        keyboardHidesTabBar: true,
+      }}
+    >
+      <Tab.Screen
+        name="Dashboard"
+        component={Dashboard}
+        options={{
+          tabBarLabel: 'Agendamentos',
+          tabBarIcon: ({ color }) => (
+            <Icon name="event" size={20} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: 'Meu Perfil',
+          tabBarIcon: ({ color }) => (
+            <Icon name="person" size={20} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
